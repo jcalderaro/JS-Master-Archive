3117d31b86f0a6f2c59b0488c3569045e5bad0d9
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-07-24 22:41:30 -0700

    Sprint

--- JS-Fundamentals/Sprint01/Challenges/functions.js
+++ JS-Fundamentals/Sprint01/Challenges/functions.js
@@ -14,7 +14,8 @@ function hof(param1, param2, cb) {
   /* Step 2: Create several functions to callback with consume();
     * Create a function named add that returns the sum of two numbers
     * Create a function named multiply that returns the product of two numbers 
-    * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+    * Create a function named greeting that accepts a first and last name 
+    * and returns "Hello first-name last-name, nice to meet you!"
   */
   
   function consume(param1, param2, cb){
@@ -34,6 +35,7 @@ function hof(param1, param2, cb) {
   };
   
   /* Step 3: Check your work by un-commenting the following calls to consume(): */
+  
   console.log(consume(2,2,add)); // 4
   console.log(consume(10,16,multiply)); // 160
   console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
