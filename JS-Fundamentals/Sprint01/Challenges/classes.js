630a5c048a2656135f1e4ac83823310833047da8
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-07-24 22:40:42 -0700

    Sprint

--- JS-Fundamentals/Sprint01/Challenges/classes.js
+++ JS-Fundamentals/Sprint01/Challenges/classes.js
@@ -1,4 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+
 class CuboidMaker1 {
     constructor(attributes){
     this.newLength = attributes.length;
@@ -16,10 +17,7 @@ class CuboidMaker1 {
 
 }
 
-
-
-
-  const cuboid1 = new CuboidMaker1({
+const cuboid1 = new CuboidMaker1({
     length: 4,
     width: 5,
     height: 5
@@ -29,7 +27,11 @@ class CuboidMaker1 {
 console.log(cuboid1.volume()); // 100
 console.log(cuboid1.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+// Find out the formulas for volume and surface area for cubes and create those 
+// methods using the dimension properties from CuboidMaker.  
+// Test your work by logging out your volume and surface area.
+
 class CubeMaker extends CuboidMaker1{
     constructor(cuboidAttr) {
         super(cuboidAttr);
